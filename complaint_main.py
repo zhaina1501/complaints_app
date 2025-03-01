844c0ae1a181af3536a1d7a95919e416c88de49d
zhaina1501 <124780460+zhaina1501@users.noreply.github.com>
2025-03-01 20:02:39 +0500

    Update complaint_main.py

--- complaint_main.py
+++ complaint_main.py
@@ -1,21 +1,8 @@
 import streamlit as st
 import os
-import time
-import webbrowser
-import pyautogui
-from time import sleep
-
-def send_whatsapp_message(phone, message):
-    """Открывает WhatsApp Web с нужным номером и текстом сообщения"""
-    url = f"https://web.whatsapp.com/send?phone={phone}&text={message}"
-    webbrowser.open(url)
-    time.sleep(20)  # Даем пользователю время нажать "Отправить"
-    pyautogui.press('enter')
 
 def main_page():
-    phone = "+77017120164"
-    message = 'HELLO TUMAR! I LOVE YOU!'
-    send_whatsapp_message(phone, message)
+    st.success("Сообщение отправлено на номер")
 
 def main():
     st.sidebar.title("Меню")
